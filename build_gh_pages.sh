771d870903b4d90d79583c17d511979b5cc2d63e
Rick Riehle <rriehle@uw.edu>
2018-02-11 17:45:53 -0700

    Test git branch push/pop

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 git_branch="git rev-parse --abbrev-ref HEAD"
 git checkout gh-pages
-rm -rf build/*
-git merge --commit --no-edit $git_branch
-touch .nojekyll  # Make sure the repo has this file in its root, otherwise it will not render on github.io
-make html
-open build/html/index.html  # On OS X this launches the rendered page into a browser; need something else for Linux or Windows
-git add *
-git commit -a --no-edit -m "Updating presentation materials"
-git pull -s ours --no-edit
-git push
-git checkout $git_branch
+# rm -rf build/*
+# git merge --commit --no-edit $git_branch
+# touch .nojekyll  # Make sure the repo has this file in its root, otherwise it will not render on github.io
+# make html
+# open build/html/index.html  # On OS X this launches the rendered page into a browser; need something else for Linux or Windows
+# git add *
+# git commit -a --no-edit -m "Updating presentation materials"
+# git pull -s ours --no-edit
+# git push
+# git checkout $git_branch
