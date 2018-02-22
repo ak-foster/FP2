4f999ba7ac5f2bef501c68509b3f404f603a841f
Rick Riehle <rriehle@uw.edu>
2018-02-22 08:55:16 -0800

    A little better

--- source/scripts/lambda-01.py
+++ source/scripts/lambda-01.py
@@ -1,28 +1,41 @@
 # Lambda tutorial
 # rriehle February 2018
 
-def add_two(x):
-    return x + 2
+def times_two(x):
+    return x * 2
 
-add_two(3)
+times_two(3)
 
-lambda x: x + 2
+lambda x: x * 2
 
-map(add_two, [x for x in range(10)])
-list(map(add_two, [x for x in range(10)]))
-list(map(lambda x: x+2, [x for x in range(10)]))
+# Lambda originated in lisp where it is mostly used with map
+# First, let's try it with the standard function.
+map(times_two, [x for x in range(10)])
 
-def mod_two(x):
-    return x % 2
+# Map returns an iterator, so let's force it to be iterated over via list.
+list(map(times_two, [x for x in range(10)]))
 
-list(map(mod_two, [x for x in range(10)]))
-list(map(lambda x: x % 2, [x for x in range(10)]))
+# Now let's use a lamdda.
+list(map(lambda x: x*2, [x for x in range(10)]))
+
+# Let's look at this in a comprehension.null=True
+[x for x in range(10)]
+[times_two(x) for x in range(10)]
+
+# Now the lambda in a coprehension
+[lambda x: x * 2 for x in range(10)]
+
+# Ugh.
+[(labda x: x * 2)(x) for x in range(10)]
+
+# Or, forget them both, you need neither the function nor the lambda.
+[x * 2 for x in range(10)]
+
 
 # Do NOT do this at home
 
-mod_two = lambda x: x % 2
+times_two = lambda x: x * 2
 
-list(map(mod_two, [x for x in range(10)]))
 
 import this
 
